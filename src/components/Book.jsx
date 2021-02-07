6481e70ccef7d485a5a994793a3247ee598486a5
Luis Antonio Canettoli <hello@luisanton.io>
2021-02-07 18:59:57 +0100

    Update Book.jsx

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -4,20 +4,21 @@ import { BookmarkStarFill } from "react-bootstrap-icons";
 import { useDispatch } from "react-redux";
 
 export default function Book({ book, index, offset, isFavourite }) {
+  const _offset = offset || 0
   const dispatch = useDispatch();
 
   return (
     <Col xs={12} className="d-flex align-items-center">
-      {Number.isInteger(index) && Number.isInteger(offset) && (
+      {Number.isInteger(index) && (
         <span className="text-muted" style={{ minWidth: 20 }}>
-          {offset + index + 1}
+          {_offset + index + 1}
         </span>
       )}
       <Image
         fluid
         src={book.img}
         style={{ width: 150 }}
-        className="my-3 mx-3"
+        className="m-3"
       />
       <div>
         <h5>{book.title}</h5>
